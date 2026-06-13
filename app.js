38ebb581c97b98cee5fd9990f5a364c281ae4350
yamadatakuya309-hue <yamada.takuya309@gmail.com>
2026-06-13 12:09:30 +0900

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 
 const config = {
   channelAccessToken: 'a9Gm8O+G17f9rIcBAiZ1cD5O1AR2jyPrDJYZfxlYbn0S9+igtU4GIyWyB2hcwSwXkflxCKifxFZGd0YuhTfqfHqxnZWgyG91K6u0X1qhPebtKfsuIrfRoL058XvAI+FLA1PwrNUwjCCm8WPmN67hwAdB04t89/1O/w1cDnyilFU=',
-channelSecret: '14bd181a88c13f4e28d0b28f6626c682'
+  channelSecret: '14bd181a88c13f4e28d0b28f6626c682'
 };
 
 const client = new line.Client(config);
@@ -20,10 +20,19 @@ app.post('/webhook', line.middleware(config), (req, res) => {
 });
 
 function handleEvent(event) {
+  if (event.type === 'join') {
+    return client.replyMessage(event.replyToken, {
+      type: 'text',
+      text: '植栽メンテナンス報告ボットです！よろしくお願いします。'
+    });
+  }
+
   if (event.type !== 'message' || event.message.type !== 'text') {
     return Promise.resolve(null);
   }
+
   const userMessage = event.message.text;
+
   return client.replyMessage(event.replyToken, {
     type: 'text',
     text: '受け取りました：' + userMessage
